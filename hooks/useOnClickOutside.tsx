6e90f6beda3a7fb6a3614e245d63759665e4c3b8
AdmiralFirefox <jjlcuadra@gmail.com>
2021-10-25 09:11:44 +0800

    Removed touchstart event on useOnClickOutside

--- hooks/useOnClickOutside.tsx
+++ hooks/useOnClickOutside.tsx
@@ -18,11 +18,9 @@ function useOnClickOutside<T extends HTMLElement = HTMLElement>(
     };
 
     document.addEventListener(`mousedown`, listener);
-    document.addEventListener(`touchstart`, listener);
 
     return () => {
       document.removeEventListener(`mousedown`, listener);
-      document.removeEventListener(`touchstart`, listener);
     };
   }, [ref, handler]);
 }
